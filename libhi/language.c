4457064a0a278ac29d481e581ca97819a10039d5
Vitaly Lipatov <lav@etersoft.ru>
2005-03-01 11:17:36 +0000

    add hi-level functions

---
+++ libhi/language.c
@@ -0,0 +1,39 @@
+/*
+    lib/get_locale.c
+
+	locale related functions
+
+    NATSPEC library
+
+    Copyright (c) 2005 Etersoft
+    Copyright (c) 2005 Vitaly Lipatov <lav@etersoft.ru>
+
+    $Id: language.c,v 1.1 2005/03/01 11:17:36 lav Exp $
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation, version 2.1
+    of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+    USA.
+*/
+
+#include <stdlib.h>
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <natspec_internal.h>
+
+const char *get_name_language(const int type, const char *locale)
+{
+	
+}
---
+++ libhi/natspechi.h
@@ -0,0 +1,45 @@
+/*
+    lib/natspec.h
+	
+	main header file for LIBNATSPEC
+
+    NATSPEC library
+    Copyright (c) 2005 Etersoft
+    Copyright (c) 2005 Vitaly Lipatov <lav@etersoft.ru>
+	http://etersoft.ru/natspec
+
+    $Id: natspechi.h,v 1.1 2005/03/01 11:17:36 lav Exp $
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation, version 2.1
+    of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+    USA.
+*/
+
+#ifndef __NATSPEC_H
+#define __NATSPEC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NATSPEC_TEX     1
+#define NATSPEC_BABEL   2
+#define NATSPEC_GUI     3
+#define NATSPEC_ISO639  4
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
