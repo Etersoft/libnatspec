ed3c0565474ad5f300d55b31e508650d2fdf47e4
Vitaly Lipatov <lav@etersoft.ru>
2005-03-02 18:26:53 +0000

    fixes for last version (detail information was lost due power failed)

--- autogen.sh
+++ autogen.sh
@@ -6,4 +6,4 @@ autoheader
 libtoolize --force --copy
 automake --add-missing --include-deps --copy --force-missing
 autoconf
-test -f Makefile && ./configure --with-debug
+test -f Makefile && ./configure --with-debug --with-python
