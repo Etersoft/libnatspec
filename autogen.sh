ba37e923c61e22f33090f3c5aa1a8648911f972d
Vitaly Lipatov <lav@etersoft.ru>
2010-01-11 01:02:11 +0300

    use -Wall only for maintainer build via autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,4 @@
 
 # We run just autoreconf, updates all needed
 autoreconf -fiv
-
-./configure --enable-maintainer-mode --with-debug --with-python
+./configure --enable-maintainer-mode --with-debug --with-python CFLAGS="-Werror -pedantic"
